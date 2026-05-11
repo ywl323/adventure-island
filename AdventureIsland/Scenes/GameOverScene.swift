c45b432229c8dfce4b89674450bcf126dd503d86
William Jarvis <william@jarvis.ai>
2026-05-11 23:17:36 +0800

    fix: correct LevelData.createDefault() instead of LevelManager.createDefault()

--- AdventureIsland/Scenes/GameOverScene.swift
+++ AdventureIsland/Scenes/GameOverScene.swift
@@ -54,7 +54,7 @@ class GameOverScene: SKScene {
         let node = self.atPoint(location)
 
         if node.name == "retryButton" {
-            let gameScene = GameScene(size: size, levelData: LevelManager.createDefault())
+            let gameScene = GameScene(size: size, levelData: LevelData.createDefault())
             gameScene.scaleMode = scaleMode
             let transition = SKTransition.flipHorizontal(withDuration: 0.5)
             view?.presentScene(gameScene, transition: transition)
