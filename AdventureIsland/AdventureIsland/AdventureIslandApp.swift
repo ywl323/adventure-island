c9d0254a091043539009fbeefe3506f62b67e5eb
William Jarvis <william@jarvis.ai>
2026-05-30 22:02:54 +0800

    fix: remove duplicate @main attribute

---
+++ AdventureIsland/AdventureIsland/AdventureIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventureIslandApp.swift
+//  AdventureIsland
+//
+//  Created by 严玮亮 on 2026/5/30.
+//
+
+import SwiftUI
+
+
+struct AdventureIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
